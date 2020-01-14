1cf7320a0aa9b92faa6fae8aab9bbd157675ead2
Hugo Valla <Hugo_valla@hotmail.fr>
2020-01-14 21:28:21 +0100

    form v1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,10 +11,10 @@ class HomeController < ApplicationController
         # re-initialize Home object for cleared form
         @contact = Home.new
         format.html { render 'index'}
-        format.js   { flash.now[:success] = @message = "Thank you for your message. I'll get back to you soon!" }
+        format.js   { flash.now[:success] = @message = "Merci Beaucoup ! On reste en contact" }
       else
         format.html { render 'index' }
-        format.js   { flash.now[:error] = @message = "Message did not send." }
+        format.js   { flash.now[:error] = @message = "hmmm... Petit problème" }
       end
     end
   end
