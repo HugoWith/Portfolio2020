78532f61d27a07eb1bcaaef390307387823a6322
Hugo Valla <Hugo_valla@hotmail.fr>
2020-01-17 15:02:24 +0100

    sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -123,4 +123,18 @@ Rails.application.configure do
     password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true  }
+
+    config.action_mailer.default_url_options = { :host => 'hugovalla.herokuapp.com' }  
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = false
+      ActionMailer::Base.smtp_settings = {
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => 'heroku.com',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+}
 end
