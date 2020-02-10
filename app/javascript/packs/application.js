37da0f6260f852d0aaed832e1188088c0eaaa7fe
Hugo Valla <Hugo_valla@hotmail.fr>
2020-02-10 14:48:34 +0100

    mofification projet

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,8 @@ require("channels");
 require("fullpage");
 
 import { initFullPageJs } from "../components/fullpage";
-import { initCustom } from "../components/custom";
+import { initCustom } from "../components/swipper";
+import { swiper } from "../components/swipper";
 // initFullPageJs();
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -18,4 +19,6 @@ import { initCustom } from "../components/custom";
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-// initCustom();
+// // initCustom();
+initFullPageJs();
+swiper();
