885eb3bd3380e7eea4e80fbfc733c21e04b4d60c
Hugo Valla <Hugo_valla@hotmail.fr>
2020-07-17 17:31:22 +0200

    réglage taille 100% écran

--- app/javascript/components/fullpage.js
+++ app/javascript/components/fullpage.js
@@ -15,9 +15,11 @@ const initFullPageJs = () => {
       const tl = new TimelineMax({ delay: 0.5 });
       tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: "-40", opacity: 1 });
       if (destination.index === 1) {
+        console.log(destination.index);
         const hugoimg = document.querySelectorAll(".hugoimg");
         const description = document.querySelector(".description");
         const revealImg = document.querySelector(".reveal-img");
+
         tl.staggerFrom(
           "#about--text",
           1.5,
@@ -48,6 +50,25 @@ const initFullPageJs = () => {
         //   .fromTo(hugoimg[0], 1, { opacity: 0.3 }, { opacity: 1 })
         //   .fromTo(hugoimg[1], 2, { opacity: 0 }, { opacity: 1 })
         //   .fromTo(hugoimg[2], 3, { opacity: 0 }, { opacity: 1 });
+      } else if (destination.index === 2) {
+        const innertextProject = document.querySelector(".innerTextPart");
+        const btnProject = document.querySelector(".btnProject");
+        tl.staggerFrom(
+          innertextProject,
+          1.5,
+          {
+            y: "1000px",
+            ease: Circ.easeOut,
+          },
+          0.3
+        );
+      } else if (destination.index === 4) {
+        const revealImgContact = document.querySelector(".reveal-img--contact");
+        tl.fromTo(
+          revealImgContact,
+          { x: "0%", duration: 0.3 },
+          { x: "100%", duration: 1.5 }
+        );
       }
     },
   });
